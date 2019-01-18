d95bcdcead693e21c66d98434dd2faad299605a9
Rodrigo Paiva <rodrigojpaiva@hotmail.com.br>
2019-01-18 18:04:17 -0300

    demo

---
+++ demo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <c-form v-model="card"></c-form>
+      <vue-card :number='card.number'
+      :mes='card.mes' :ano='card.ano'
+      :nome='card.nome' :cvv='card.cvv'
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import vueCard from 'vue-card-express'
+import cForm from './components/form'
+export default {
+  components: {
+    cForm, vueCard
+  },
+  name: 'app',
+  data () {
+    return {
+      card: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  color: #2c3e50;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ddd;
+  padding: 25px;
+  border-radius: 5px;
+  border: 1px solid #ccc
+}
+</style>
---
+++ demo/src/components/form.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="Demo">
+      <h1>Demo - vue card express</h1>
+      <div id="form">
+       <div class="number">
+          <div class="numero">
+            <label for="">Numero</label>
+            <input type="number" v-model='card.number'>
+          </div>
+          <div class="data">
+            <div class="mes">
+                <label for="">Mês</label>
+                <input type="number" v-model='card.mes'>
+            </div>
+            <div class="ano">
+              <label for="">Ano</label>
+              <input type="number" v-model='card.ano'>
+            </div>
+          </div>
+        </div>
+        <div class="nome">
+            <div class="nomecartao">
+                <label for="">Nome</label>
+                <input type="text" v-model='card.nome'>
+            </div>
+            <div class="data">
+                <label for="">CVV</label>
+                <input type="number" v-model='card.cvv'>
+            </div>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      required: true
+    }
+  },
+  data: () => ({
+    card: {
+    }
+  }),
+  mounted () {
+    this.card = {
+      number: '000000000000000',
+      ano: '2018',
+      cvv: '085',
+      mes: '05',
+      nome: 'WEBPAGO SA'
+    }
+  },
+  watch: {
+    value () {
+      this.value = this.card
+    },
+    card () {
+      this.$emit('input', this.card)
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.Demo
+  h1
+    color red
+  #form
+    padding 20px
+    background: #eee
+    margin 10px
+    label
+      margin 5px
+      margin-left 0px
+      text-align: left
+    .number
+      display flex
+      justify-content: center;
+      align-items: center;
+      label
+        margin 5px
+        margin-left 0px
+      .numero
+        width 80%
+        display flex
+        justify-content: center;
+        flex-direction:column;
+        text-align: left
+        padding 10px
+      .data
+        display flex
+        justify-content: center;
+        align-items: center;
+        text-align: left
+        input
+          max-width:50px
+        .mes
+          display flex
+          justify-content: center;
+          flex-direction: column;
+          text-align: left;
+          padding 10px
+          label
+            margin 5px
+            margin-left 0px
+            text-align: left
+        .ano
+          display flex
+          justify-content: center;
+          flex-direction: column;
+          text-align: left;
+          padding 10px
+          label
+            margin 5px
+            margin-left 0px
+            text-align: left
+    .nome
+      display flex
+      justify-content: center;
+      align-items: center;
+      label
+        margin 5px
+        margin-left 0px
+      .nomecartao
+        display flex
+        justify-content: center;
+        flex-direction: column;
+        padding 10px
+        width 90%
+        label
+          margin 5px
+          margin-left 0px
+      .data
+        display flex
+        justify-content: center;
+        flex-direction: column;
+        padding 10px
+        label
+          margin 5px
+          margin-left 0px
+        input
+          max-width: 50px
+</style>
